84a4485632b7393af28cedb39549a4ed3d750c63
Leti <leticiapeuser@gmail.com>
2019-02-12 15:37:59 -0300

    index / image gallery / cta buttons / js for show hide message

---
+++ javascripts/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".expand").click(function(){$(this).hide(),$(this).parent().find(".collapse").show(),$(this).parent().find(".comment").addClass("show")}),$(".collapse").click(function(){$(this).hide(),$(this).parent().find(".expand").show(),$(this).parent().find(".comment").removeClass("show")})});
